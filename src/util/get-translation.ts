6a527addbb0336d50655fcb998d64872aad6e29f
Johnny Gérard <john@jgerard.dev>
2025-02-21 20:32:31 +0100

    feat: add get-translation.ts

---
+++ src/util/get-translation.ts
@@ -0,0 +1,17 @@
+import { AppLocale } from "@/type/app-locale";
+
+// This object is structured specifically to allow the TypeScript system to
+// properly infer the JSON data types.
+const translations = {
+  "en-US": () =>
+    import("../../data/translation/en-US.json").then(
+      (module) => module.default,
+    ),
+  "fr-FR": () =>
+    import("../../data/translation/fr-FR.json").then(
+      (module) => module.default,
+    ),
+};
+
+export const getTranslation = async (locale: AppLocale) =>
+  translations[locale]();
