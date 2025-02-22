03b4314935ea6f46dc02e16e195c46bcbd8f5f12
Johnny Gérard <john@jgerard.dev>
2025-02-22 02:50:04 +0100

    feat: add homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,31 @@
+import PreviewCard from "@/component/preview-card";
+import { AppLocale } from "@/type/app-locale";
+import { getTranslation } from "@/util/get-translation";
 import { memo } from "react";
 
-const HomePage = () => {
-  return <div></div>;
+type Props = {
+  params: Promise<{ locale: AppLocale }>;
 };
 
-export default memo(HomePage);
+const Page = async ({ params }: Props) => {
+  const { locale } = await params;
+  const text = await getTranslation(locale);
+  const imageNames = ["sedan", "suv", "luxury"];
+
+  return (
+    <ul>
+      {text.vehicles.map((vehicle, index) => (
+        <li key={vehicle.type}>
+          <PreviewCard
+            imageName={imageNames[index]}
+            heading={vehicle.type}
+            description={vehicle.description}
+            learnMore={text.learnMore}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default memo(Page);
