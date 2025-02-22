5c1b3455be31b72a71acd38cfb79c1f5f7cbf5ee
Johnny Gérard <john@jgerard.dev>
2025-02-22 07:31:01 +0100

    refactor: improve getTranslation

--- src/util/get-translation.ts
+++ src/util/get-translation.ts
@@ -1,23 +1,21 @@
 import "server-only";
-import { AVAILABLE_LOCALES } from "@/constant/i18n";
 import { AppLocale } from "@/type/app-locale";
-import { notFound } from "next/navigation";
-
-// This object is structured specifically to allow the TypeScript system to
-// properly infer the JSON data types.
-const translations = {
-  "en-US": () =>
-    import("../../data/translation/en-US.json").then(
-      (module) => module.default,
-    ),
-  "fr-FR": () =>
-    import("../../data/translation/fr-FR.json").then(
-      (module) => module.default,
-    ),
-};
 
 export const getTranslation = async (locale: AppLocale) => {
-  // This check is required for certain paths such as `/go/he.re`
-  if (!AVAILABLE_LOCALES.includes(locale)) notFound();
-  return translations[locale]();
+  // This code is structured specifically to allow the TypeScript system to
+  // properly infer the JSON data types.
+  switch (locale) {
+    case "en-US":
+      return import("../../data/translation/en-US.json").then(
+        (module) => module.default,
+      );
+    case "fr-FR":
+      return import("../../data/translation/fr-FR.json").then(
+        (module) => module.default,
+      );
+    default:
+      return import("../../data/translation/en-US.json").then(
+        (module) => module.default,
+      );
+  }
 };
